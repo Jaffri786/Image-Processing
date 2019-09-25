015196f22614a92643865ea62e1f3edf1751752a
Jaffri786 <55681253+Jaffri786@users.noreply.github.com>
2019-09-25 12:37:12 -0700

    Update Blue_Channel.py

--- Blue_Channel.py
+++ Blue_Channel.py
@@ -5,17 +5,18 @@ Created on Tue Sep 17 14:10:35 2019
 @author: Ali Raza
 """
 
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #library for plotting
 import cv2
 
 #Blue chanel image
 
-img=cv2.imread('C:/Users/Ali Raza/Desktop/Python project/RGB.jpg')
-img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+img=cv2.imread('C:/Users/Ali Raza/Desktop/Python project/RGB.jpg') #image reading fucntion give the path of image
 
-frame=plt.figure()
+img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB) #function for change image clr from BGR to RGB because default image read as BGR
 
-frame.add_subplot(1,2,1)
+frame=plt.figure() #initialize frames
+
+frame.add_subplot(1,2,1) #(Rows,coloumn,image no.)
 plt.imshow(img)
 plt.axis('off')
 plt.title('Original Image')
